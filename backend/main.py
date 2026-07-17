24af4968655f0565faa8a78d94205de0853276f7
infinity889 <kabijan.nurbol@gmail.com>
2026-07-17 22:13:37 +0500

    feat: added patch and delete endpoint

--- backend/main.py
+++ backend/main.py
@@ -19,6 +19,9 @@ class TaskSchema(BaseModel):
 class TAskCreateSchema(BaseModel):
     title: str
 
+class TAskUpdateSchema(BaseModel):
+    title: str | None = None
+    completed: bool | None = None
 
 tasks: list[TaskSchema] = []
 
@@ -26,12 +29,31 @@ tasks: list[TaskSchema] = []
 def get_tasks():
     return tasks
 
-@app.post("/tasks")
-def create_task(task: TAskCreateSchema):
-    new_task = TaskSchema(id=str(uuid4()), title=task.title, completed=False)
+@app.post("/tasks", response_model=TaskSchema)
+def create_task(payload: TAskCreateSchema):
+    new_task = TaskSchema(id=str(uuid4()), title=payload.title, completed=False)
     tasks.append(new_task)
     return new_task
 
 
+@app.patch("/tasks/{task_id}", response_model=TaskSchema)
+def update_task(payload: TAskUpdateSchema, task_id: str):
+    for task in tasks:
+        if task.id == task_id:
+            if payload.title is not None:
+                task.title = payload.title
+            if payload.completed is not None:
+                task.completed = payload.completed
+            return task
+    return {"error": "Task not found"}
+
+
+@app.delete("/tasks/{task_id}", response_model=dict)
+def delete_task(task_id: str):
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
+            return {"message": "Task deleted"}
+    return {"error": "Task not found"}
 
 
